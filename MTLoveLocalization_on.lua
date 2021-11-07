00f59a86741f60cabb28b310ea06218a347a5607
dartraiden <wowemuh@gmail.com>
2021-11-08 01:24:51 +0300

    Add Russian localization

--- MTLoveLocalization_on.lua
+++ MTLoveLocalization_on.lua
@@ -4,6 +4,7 @@ MTLove_UI_Localization	= "default";
 	"default"		-> the local-ID from your client
 	"enUS"/"enGB"	-> english
 	"deDE"			-> german
+	"ruRU"			-> russian
 
 	If here is no localization for your language or its is
 	outdated and disabled and you want add or update it,
